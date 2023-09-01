49ff3091c13c1de4fbb8ff074465b8416f5acb62
mercierclifford9 <140321835+mercierclifford9@users.noreply.github.com>
2023-09-01 18:07:03 -0400

    Add files via upload

--- UrlPush.js
+++ UrlPush.js
@@ -1,35 +1,56 @@
 
 
-class UrlPush {
+class URLPush  {
     constructor() {
         this.params = new URLSearchParams(window.location.search);
     }
 
     push(kle, vale) {
+        if (!this.params.has(kle)) {
         this.params.set(kle, vale);
-        this.updateURL();
+        let nouvelleUrl = `${window.location.pathname}?${this.params.toString()}`;
+        window.history.pushState({}, '', nouvelleUrl);
+        }else{
+            throw new Error (`la cles"${kle}" existe deja`);
+        }
     }
+    
     update(kle, vale){
-        this.params.set(kle, vale);
-        this.updateURL();
+        if (this.params.has(kle)) {
+            this.params.set(kle, vale);
+            let nouvelleUrl = `${window.location.pathname}?${this.params.toString()}`;
+            window.history.pushState({}, '', nouvelleUrl);
+        
+        }else{
+            throw new Error (`la cles"${kle}" n'existe pas`);
+        }
         
     }
     get(kle) {
-        return this.params.get(kle);
+        if (this.params.has(kle)){
+            return this.params.get(kle);
+        }else{
+            throw new Error (`la cles"${kle}" n'existe pas`);
+        }
+        
     }
 
     remove(kle) {
-        this.params.delete(kle);
-        this.updateURL();
+        if (this.params.has(kle)) {
+            this.params.delete(kle);
+            let nouvelleUrl = `${window.location.pathname}?${this.params.toString()}`;
+            window.history.pushState({}, '', nouvelleUrl);
+        
+        }else{
+            throw new Error (`la cles"${kle}" n'existe pas`);
+        }
+       
     }
-   
-    getParamKeys() {
-        return Array.from(this.params.keys());
-    }
-    updateURL() {
-        let nouvelleUrl = `${window.location.pathname}?${this.params.toString()}`;
-        window.history.pushState({}, '', nouvelleUrl);
-    }
-    
 }
 
+let url = new URLPush()
+// url.push('.', 'Value');
+url.update('.', 'messi')
+// url.get('search')
+// url.remove('go')
+// console.log(url);
